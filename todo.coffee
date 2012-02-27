c03424337aac1f09fee493c08dc3accab4d20b02
Elf M. Sternberg <elf@pendorwright.com>
2012-02-27 07:10:34 -0800

    Spineless task management works.  Category management, not so much.

--- todo.coffee
+++ todo.coffee
@@ -1,110 +1,168 @@
-class Category extends Backbone.Model
+class RightNow
 
-class CategoryList extends Backbone.Collection
-        lawnchair: new Lawnchair({ name: "RightNowCategoryList"}, new Function())
-
-class CategoryListView extends Backbone.View
-    template: $("#todo-template").html()
-    editTemplate: $('#todo-edit-template').html()
-    el: $('#rightnow')
-
-    events:
-        "mouseover div.category": "showAddButton"
-        "mouseout div.category": "hideAddButton"
-        "mouseover h3.newcat": "showAddButton"
-        "mouseout div.category": "hideAddButton"
-        "click .task": "nowEditTask"
-        "keyup .editing .edit-task-field": "maybeTaskSave"
-
-    initialize: (options) ->
-        @collection.bind('change', @render)
-
-    showAddButton: (ev) ->
-        $("button.addcat", ev.currentTarget).fadeIn('fast')
-
-    hideAddButton: (ev) ->
-        $("button.addcat", ev.currentTarget).fadeOut('fast')
+    constructor: (@repo) ->
+        @repo.get 'rightnow', (cat) =>
+            @todos = cat.rightnow
+            @render()
+            $('body').on 'click', @endEdits
 
     catAndTask: (t) ->
-        t = t.get(0)
-        t = if t.tagName.toUpperCase() == 'LI' then t else $(t).closest('li.task')
-        $(t).attr('id').split('-')[1..2]
+        (parseInt(i) for i in $(t).attr('id').split('-')[1..2])
 
     catOnly: (t) ->
-        t = if t.tagName.toUpperCase() == 'LI' then t else $(t).closest('li.category')
-        $(t).attr('id').split('-')[1]
+        parseInt($(t).attr('id').split('-')[1])
 
-    # Restores a task back to its normal view.
+    cat_render: (cat) ->
+        cat_enumerate = (c) -> ([i, c[i].name, c[i].tasks] for i in [0..c.length - 1])
+        cat_renderer = (c, r) ->
+            "<li id=\"cat-#{i[0]}\"><div class=\"catcontainer\"><div class=\"category\">#{i[1]}</div>" + \
+            "<button class=\"fadebutton addstory\" style=\"display:none\">+ New Project</button>" + \
+            "<button class=\"fadebutton editcat\" style=\"display:none\">Edit Category</button></div>#{r}</li>"
 
-    displayTaskView: (el) ->
-        $(el).removeClass('editing')
-        [category, task] = @catAndTask(el)
-        $(el).html (@collection.at(category).get('tasks')[task])
+        task_enumerate = (t) -> ([i, t[i]] for i in [0..t.length - 1])
+        tasks_renderer = (cid, tid, task) ->
+            "<li id=\"cat-#{cid}-#{tid}\" class=\"task\"><div>#{task}</div></li>"
 
-    # If the user clicks outside the editable, all editable
-    # are reset.
+        tasks_render = (cid, tasks) ->
+            return "" if not tasks.length
+            "<ul>" + (tasks_renderer(cid, i[0], i[1]) for i in task_enumerate(tasks)).join("") + "</ul>"
 
-    registerUnclick: ->
-        return if @registered
-        @registered = true
-        $('#content').click (ev) =>
-            ev.preventDefault()
-            editing = $('li.editing')
-            editing.each (i, el) =>
-                @displayTaskView($(el))
-            $('bind').unbind('click')
-            @registered = false
+        "<ul>" + (cat_renderer(i, tasks_render(i[0], i[2])) for i in cat_enumerate(cat)).join("") + "</ul>"
 
-    # Sets a task to its edit view
+    showAddButton: (ev) =>
+        ev.stopPropagation()
+        tg = $("button.fadebutton", ev.currentTarget.parentNode)
+        tg.fadeIn('fast')
+        clearTimeout(tg.data("fader")) if tg.data("fader")
+        tg.data("fader", setTimeout (() -> if tg.css("display") != "none" then tg.fadeOut('fast')), 2500)
 
-    editTaskView: (tg) =>
-        return if tg.hasClass('editing')
-        $('li.editing').each (i, el) => @displayTaskView($(el))
-        tg.addClass('editing')
-        @registerUnclick()
-        [category, task] = @catAndTask(tg)
-        $(tg).html _.template @editTemplate,
-            task: @collection.at(category).get('tasks')[task]
-            taskid: task
-            catid: category
-        $('input.edit-task-field', tg).focus()
+    endEdits: =>
+        $('li.editing').each (i, el) =>
+            $(el).removeClass('editing')
+            [cid, tid] = @catAndTask(el)
+            if not @todos[cid].tasks[tid]?
+                $(el).remove()
+            else
+                $(el).html (@todos[cid].tasks[tid])
 
-    # Save the content of a task on a save event
+    cleanAndEndEdits: ->
+        @todos = ({name: c.name, tasks: (t for t in c.tasks when t.trim() != "")} for c in @todos when c.name.trim() != "")
+        @endEdits()
+
+    save: ->
+        @repo.save {key: 'rightnow', 'rightnow': @todos}, () =>
+            @render()
 
     taskSave: (ev, tg) ->
-        input = tg.val()
-        [category, task] = @catAndTask(tg)
-        category = @collection.at(category)
-        tasks = category.get('tasks')
-        if input.trim == ""
-            category.set('tasks', tasks.slice(0, task).append(tasks.slice(task + 1, tasks.length)))
-            @render()
+        val = tg.val()
+        [cid, tid] = @catAndTask(tg)
+        t = @todos[cid].tasks
+        @todos[cid].tasks = if val.trim() == ""
+            t.slice(0, tid).concat(t.slice(tid + 1, t.length))
         else
-            tasks[task] = input
-            category.set('tasks', tasks)
-            category.save({silent: true})
-            @displayTaskView(tg.closest('li'))
+            t.slice(0, tid).concat([val]).concat(t.slice(tid + 1))
+        @save()
 
-    maybeTaskSave: (ev) ->
+    maybeTaskSave: (ev) =>
+        console.log(ev)
         code = if ev.keyCode then ev.keyCode else ev.which
-        console.log(code)
         tg = $(ev.currentTarget)
-        [category, task] = @catAndTask(tg)
+        [cid, tid] = @catAndTask(tg)
         return @taskSave(ev, tg) if code == 13
-        return @displayTaskView($(tg).closest('li')) if code == 27
+        return @cleanAndEndEdits() if code == 27
 
-    nowEditTask: (ev) ->
+    deleteTask: (ev) =>
         ev.stopPropagation()
-        tg = $(ev.currentTarget).closest('li')
-        @editTaskView(tg)
+        tg = $(ev.currentTarget)
+        console.log(tg)
+        [cid, tid] = @catAndTask(tg)
+        t = @todos[cid].tasks
+        @todos[cid].tasks = t.slice(0, tid).concat(t.slice(tid + 1, t.length))
+        @save()
 
-    render: =>
-        $('#todos', @el).html(_.template(@template, {'categories': @collection.toJSON()}))
+    editRender: (tg) ->
+        return if tg.hasClass('editing')
+
+        @endEdits()
+
+        [cid, tid] = @catAndTask(tg)
+        return if cid == null or cid == null
+
+        task = @todos[cid].tasks[tid]
+        return if task == null
+
+        console.log(tg, cid, tid, "[" + task + "]")
+
+        edit_render = (cid, tid, task) ->
+            "<div class=\"edit-task\">" + \
+            "<input type=\"text\" value=\"#{task}\" id=\"edit-#{cid}-#{tid}\" " + \
+            "class=\"edit-task-field\" />" + \
+            "<button class=\"delete-task-field\" id=\"del-#{cid}-#{tid}\">&#x02A2F;</button>" + \
+            "</div>"
+
+        tg.addClass('editing')
+        tg.html edit_render(cid, tid, task)
+
+        $('input.edit-task-field', tg).on 'keyup', @maybeTaskSave
+        $('.edit-task', tg).on 'click', (ev) => ev.stopPropagation()
+        $('.delete-task-field', tg).on 'click', @deleteTask
+        $('input.edit-task-field', tg).focus()
+
+    editCatRender: (tg) ->
+        return if tg.hasClass('editing')
+
+        @endEdits()
+
+        [cid, tid] = @catAndTask(tg)
+        return if cid == null or cid == null
+
+        task = @todos[cid].tasks[tid]
+        return if task == null
+
+        console.log(tg, cid, tid, "[" + task + "]")
+
+        edit_render = (cid, tid, task) ->
+            "<div class=\"edit-task\">" + \
+            "<input type=\"text\" value=\"#{task}\" id=\"edit-#{cid}-#{tid}\" " + \
+            "class=\"edit-task-field\" />" + \
+            "<button class=\"delete-task-field\" id=\"del-#{cid}-#{tid}\">&#x02A2F;</button>" + \
+            "</div>"
+
+        tg.addClass('editing')
+        tg.html edit_render(cid, tid, task)
+
+        $('input.edit-task-field', tg).on 'keyup', @maybeTaskSave
+        $('.edit-task', tg).on 'click', (ev) => ev.stopPropagation()
+        $('.delete-task-field', tg).on 'click', @deleteTask
+        $('input.edit-task-field', tg).focus()
+
+
+    newTask: (ev) =>
+        ev.stopPropagation()
+        tg = $($(ev.currentTarget).closest('li'))
+        $('.fadebutton', tg).hide()
+        cid = @catOnly(tg)
+        tid = @todos[cid].tasks.length
+        @todos[cid].tasks.push("")
+        $("li#cat-#{cid} ul").append("<li id=\"cat-#{cid}-#{tid}\" class=\"task\"></li>")
+        setTimeout (() => @editRender($("#cat-#{cid}-#{tid}"))), 10
+
+    editTask: (ev) =>
+        ev.stopPropagation()
+        tg = $(ev.currentTarget)
+        @editRender(tg)
+
+    connect: ->
+
+    render: ->
+        $('#todos').html(@cat_render(@todos))
+        $('div.category').on 'click', @showAddButton
+        $('#newcat h1').on 'click', @showAddButton
+        $('li.task').on 'click', @editTask
+        $('.addstory').on 'click', @newTask
+
 
 $ ->
-    categoryList = new CategoryList()
-    categoryList.fetch
-        success: (categoryList) ->
-            view = new CategoryListView {collection: categoryList}
-            view.render()
+    rightnow = new Lawnchair {name: 'RightNow'}, (rightnow) ->
+        handler = new RightNow(rightnow)
 
--- todo.js
+++ todo.js
@@ -1,181 +1,273 @@
 (function() {
-  var Category, CategoryList, CategoryListView;
-  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; }, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  var RightNow;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-  Category = (function() {
+  RightNow = (function() {
 
-    __extends(Category, Backbone.Model);
-
-    function Category() {
-      Category.__super__.constructor.apply(this, arguments);
-    }
-
-    return Category;
-
-  })();
-
-  CategoryList = (function() {
-
-    __extends(CategoryList, Backbone.Collection);
-
-    function CategoryList() {
-      CategoryList.__super__.constructor.apply(this, arguments);
-    }
-
-    CategoryList.prototype.lawnchair = new Lawnchair({
-      name: "RightNowCategoryList"
-    }, new Function());
-
-    return CategoryList;
-
-  })();
-
-  CategoryListView = (function() {
-
-    __extends(CategoryListView, Backbone.View);
-
-    function CategoryListView() {
-      this.render = __bind(this.render, this);
-      this.editTaskView = __bind(this.editTaskView, this);
-      CategoryListView.__super__.constructor.apply(this, arguments);
-    }
-
-    CategoryListView.prototype.template = $("#todo-template").html();
-
-    CategoryListView.prototype.editTemplate = $('#todo-edit-template').html();
-
-    CategoryListView.prototype.el = $('#rightnow');
-
-    CategoryListView.prototype.events = {
-      "mouseover div.category": "showAddButton",
-      "mouseout div.category": "hideAddButton",
-      "mouseover h3.newcat": "showAddButton",
-      "mouseout div.category": "hideAddButton",
-      "click .task": "nowEditTask",
-      "keyup .editing .edit-task-field": "maybeTaskSave"
-    };
-
-    CategoryListView.prototype.initialize = function(options) {
-      return this.collection.bind('change', this.render);
-    };
-
-    CategoryListView.prototype.showAddButton = function(ev) {
-      return $("button.addcat", ev.currentTarget).fadeIn('fast');
-    };
-
-    CategoryListView.prototype.hideAddButton = function(ev) {
-      return $("button.addcat", ev.currentTarget).fadeOut('fast');
-    };
-
-    CategoryListView.prototype.catAndTask = function(t) {
-      t = t.get(0);
-      t = t.tagName.toUpperCase() === 'LI' ? t : $(t).closest('li.task');
-      return $(t).attr('id').split('-').slice(1, 3);
-    };
-
-    CategoryListView.prototype.catOnly = function(t) {
-      t = t.tagName.toUpperCase() === 'LI' ? t : $(t).closest('li.category');
-      return $(t).attr('id').split('-')[1];
-    };
-
-    CategoryListView.prototype.displayTaskView = function(el) {
-      var category, task, _ref;
-      $(el).removeClass('editing');
-      _ref = this.catAndTask(el), category = _ref[0], task = _ref[1];
-      return $(el).html((this.collection.at(category).get('tasks')[task]));
-    };
-
-    CategoryListView.prototype.registerUnclick = function() {
+    function RightNow(repo) {
       var _this = this;
-      if (this.registered) return;
-      this.registered = true;
-      return $('#content').click(function(ev) {
-        var editing;
-        ev.preventDefault();
-        editing = $('li.editing');
-        editing.each(function(i, el) {
-          return _this.displayTaskView($(el));
-        });
-        $('bind').unbind('click');
-        return _this.registered = false;
+      this.repo = repo;
+      this.editTask = __bind(this.editTask, this);
+      this.newTask = __bind(this.newTask, this);
+      this.deleteTask = __bind(this.deleteTask, this);
+      this.maybeTaskSave = __bind(this.maybeTaskSave, this);
+      this.endEdits = __bind(this.endEdits, this);
+      this.showAddButton = __bind(this.showAddButton, this);
+      this.repo.get('rightnow', function(cat) {
+        _this.todos = cat.rightnow;
+        _this.render();
+        return $('body').on('click', _this.endEdits);
+      });
+    }
+
+    RightNow.prototype.catAndTask = function(t) {
+      var i, _i, _len, _ref, _results;
+      _ref = $(t).attr('id').split('-').slice(1, 3);
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        i = _ref[_i];
+        _results.push(parseInt(i));
+      }
+      return _results;
+    };
+
+    RightNow.prototype.catOnly = function(t) {
+      return parseInt($(t).attr('id').split('-')[1]);
+    };
+
+    RightNow.prototype.cat_render = function(cat) {
+      var cat_enumerate, cat_renderer, i, task_enumerate, tasks_render, tasks_renderer;
+      cat_enumerate = function(c) {
+        var i, _ref, _results;
+        _results = [];
+        for (i = 0, _ref = c.length - 1; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+          _results.push([i, c[i].name, c[i].tasks]);
+        }
+        return _results;
+      };
+      cat_renderer = function(c, r) {
+        return ("<li id=\"cat-" + i[0] + "\"><div class=\"catcontainer\"><div class=\"category\">" + i[1] + "</div>") + "<button class=\"fadebutton addstory\" style=\"display:none\">+ New Project</button>" + ("<button class=\"fadebutton editcat\" style=\"display:none\">Edit Category</button></div>" + r + "</li>");
+      };
+      task_enumerate = function(t) {
+        var i, _ref, _results;
+        _results = [];
+        for (i = 0, _ref = t.length - 1; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+          _results.push([i, t[i]]);
+        }
+        return _results;
+      };
+      tasks_renderer = function(cid, tid, task) {
+        return "<li id=\"cat-" + cid + "-" + tid + "\" class=\"task\"><div>" + task + "</div></li>";
+      };
+      tasks_render = function(cid, tasks) {
+        var i;
+        if (!tasks.length) return "";
+        return "<ul>" + ((function() {
+          var _i, _len, _ref, _results;
+          _ref = task_enumerate(tasks);
+          _results = [];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            i = _ref[_i];
+            _results.push(tasks_renderer(cid, i[0], i[1]));
+          }
+          return _results;
+        })()).join("") + "</ul>";
+      };
+      return "<ul>" + ((function() {
+        var _i, _len, _ref, _results;
+        _ref = cat_enumerate(cat);
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          i = _ref[_i];
+          _results.push(cat_renderer(i, tasks_render(i[0], i[2])));
+        }
+        return _results;
+      })()).join("") + "</ul>";
+    };
+
+    RightNow.prototype.showAddButton = function(ev) {
+      var tg;
+      ev.stopPropagation();
+      tg = $("button.fadebutton", ev.currentTarget.parentNode);
+      tg.fadeIn('fast');
+      if (tg.data("fader")) clearTimeout(tg.data("fader"));
+      return tg.data("fader", setTimeout((function() {
+        if (tg.css("display") !== "none") return tg.fadeOut('fast');
+      }), 2500));
+    };
+
+    RightNow.prototype.endEdits = function() {
+      var _this = this;
+      return $('li.editing').each(function(i, el) {
+        var cid, tid, _ref;
+        $(el).removeClass('editing');
+        _ref = _this.catAndTask(el), cid = _ref[0], tid = _ref[1];
+        if (!(_this.todos[cid].tasks[tid] != null)) {
+          return $(el).remove();
+        } else {
+          return $(el).html(_this.todos[cid].tasks[tid]);
+        }
       });
     };
 
-    CategoryListView.prototype.editTaskView = function(tg) {
-      var category, task, _ref;
+    RightNow.prototype.cleanAndEndEdits = function() {
+      var c, t;
+      this.todos = (function() {
+        var _i, _len, _ref, _results;
+        _ref = this.todos;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          c = _ref[_i];
+          if (c.name.trim() !== "") {
+            _results.push({
+              name: c.name,
+              tasks: (function() {
+                var _j, _len2, _ref2, _results2;
+                _ref2 = c.tasks;
+                _results2 = [];
+                for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+                  t = _ref2[_j];
+                  if (t.trim() !== "") _results2.push(t);
+                }
+                return _results2;
+              })()
+            });
+          }
+        }
+        return _results;
+      }).call(this);
+      return this.endEdits();
+    };
+
+    RightNow.prototype.save = function() {
+      var _this = this;
+      return this.repo.save({
+        key: 'rightnow',
+        'rightnow': this.todos
+      }, function() {
+        return _this.render();
+      });
+    };
+
+    RightNow.prototype.taskSave = function(ev, tg) {
+      var cid, t, tid, val, _ref;
+      val = tg.val();
+      _ref = this.catAndTask(tg), cid = _ref[0], tid = _ref[1];
+      t = this.todos[cid].tasks;
+      this.todos[cid].tasks = val.trim() === "" ? t.slice(0, tid).concat(t.slice(tid + 1, t.length)) : t.slice(0, tid).concat([val]).concat(t.slice(tid + 1));
+      return this.save();
+    };
+
+    RightNow.prototype.maybeTaskSave = function(ev) {
+      var cid, code, tg, tid, _ref;
+      console.log(ev);
+      code = ev.keyCode ? ev.keyCode : ev.which;
+      tg = $(ev.currentTarget);
+      _ref = this.catAndTask(tg), cid = _ref[0], tid = _ref[1];
+      if (code === 13) return this.taskSave(ev, tg);
+      if (code === 27) return this.cleanAndEndEdits();
+    };
+
+    RightNow.prototype.deleteTask = function(ev) {
+      var cid, t, tg, tid, _ref;
+      ev.stopPropagation();
+      tg = $(ev.currentTarget);
+      console.log(tg);
+      _ref = this.catAndTask(tg), cid = _ref[0], tid = _ref[1];
+      t = this.todos[cid].tasks;
+      this.todos[cid].tasks = t.slice(0, tid).concat(t.slice(tid + 1, t.length));
+      return this.save();
+    };
+
+    RightNow.prototype.editRender = function(tg) {
+      var cid, edit_render, task, tid, _ref;
       var _this = this;
       if (tg.hasClass('editing')) return;
-      $('li.editing').each(function(i, el) {
-        return _this.displayTaskView($(el));
-      });
+      this.endEdits();
+      _ref = this.catAndTask(tg), cid = _ref[0], tid = _ref[1];
+      if (cid === null || cid === null) return;
+      task = this.todos[cid].tasks[tid];
+      if (task === null) return;
+      console.log(tg, cid, tid, "[" + task + "]");
+      edit_render = function(cid, tid, task) {
+        return "<div class=\"edit-task\">" + ("<input type=\"text\" value=\"" + task + "\" id=\"edit-" + cid + "-" + tid + "\" ") + "class=\"edit-task-field\" />" + ("<button class=\"delete-task-field\" id=\"del-" + cid + "-" + tid + "\">&#x02A2F;</button>") + "</div>";
+      };
       tg.addClass('editing');
-      this.registerUnclick();
-      _ref = this.catAndTask(tg), category = _ref[0], task = _ref[1];
-      $(tg).html(_.template(this.editTemplate, {
-        task: this.collection.at(category).get('tasks')[task],
-        taskid: task,
-        catid: category
-      }));
+      tg.html(edit_render(cid, tid, task));
+      $('input.edit-task-field', tg).on('keyup', this.maybeTaskSave);
+      $('.edit-task', tg).on('click', function(ev) {
+        return ev.stopPropagation();
+      });
+      $('.delete-task-field', tg).on('click', this.deleteTask);
       return $('input.edit-task-field', tg).focus();
     };
 
-    CategoryListView.prototype.taskSave = function(ev, tg) {
-      var category, input, task, tasks, _ref;
-      input = tg.val();
-      _ref = this.catAndTask(tg), category = _ref[0], task = _ref[1];
-      category = this.collection.at(category);
-      tasks = category.get('tasks');
-      if (input.trim === "") {
-        category.set('tasks', tasks.slice(0, task).append(tasks.slice(task + 1, tasks.length)));
-        return this.render();
-      } else {
-        tasks[task] = input;
-        category.set('tasks', tasks);
-        category.save({
-          silent: true
-        });
-        return this.displayTaskView(tg.closest('li'));
-      }
+    RightNow.prototype.editCatRender = function(tg) {
+      var cid, edit_render, task, tid, _ref;
+      var _this = this;
+      if (tg.hasClass('editing')) return;
+      this.endEdits();
+      _ref = this.catAndTask(tg), cid = _ref[0], tid = _ref[1];
+      if (cid === null || cid === null) return;
+      task = this.todos[cid].tasks[tid];
+      if (task === null) return;
+      console.log(tg, cid, tid, "[" + task + "]");
+      edit_render = function(cid, tid, task) {
+        return "<div class=\"edit-task\">" + ("<input type=\"text\" value=\"" + task + "\" id=\"edit-" + cid + "-" + tid + "\" ") + "class=\"edit-task-field\" />" + ("<button class=\"delete-task-field\" id=\"del-" + cid + "-" + tid + "\">&#x02A2F;</button>") + "</div>";
+      };
+      tg.addClass('editing');
+      tg.html(edit_render(cid, tid, task));
+      $('input.edit-task-field', tg).on('keyup', this.maybeTaskSave);
+      $('.edit-task', tg).on('click', function(ev) {
+        return ev.stopPropagation();
+      });
+      $('.delete-task-field', tg).on('click', this.deleteTask);
+      return $('input.edit-task-field', tg).focus();
     };
 
-    CategoryListView.prototype.maybeTaskSave = function(ev) {
-      var category, code, task, tg, _ref;
-      code = ev.keyCode ? ev.keyCode : ev.which;
-      console.log(code);
-      tg = $(ev.currentTarget);
-      _ref = this.catAndTask(tg), category = _ref[0], task = _ref[1];
-      if (code === 13) return this.taskSave(ev, tg);
-      if (code === 27) return this.displayTaskView($(tg).closest('li'));
+    RightNow.prototype.newTask = function(ev) {
+      var cid, tg, tid;
+      var _this = this;
+      ev.stopPropagation();
+      tg = $($(ev.currentTarget).closest('li'));
+      $('.fadebutton', tg).hide();
+      cid = this.catOnly(tg);
+      tid = this.todos[cid].tasks.length;
+      this.todos[cid].tasks.push("");
+      $("li#cat-" + cid + " ul").append("<li id=\"cat-" + cid + "-" + tid + "\" class=\"task\"></li>");
+      return setTimeout((function() {
+        return _this.editRender($("#cat-" + cid + "-" + tid));
+      }), 10);
     };
 
-    CategoryListView.prototype.nowEditTask = function(ev) {
+    RightNow.prototype.editTask = function(ev) {
       var tg;
       ev.stopPropagation();
-      tg = $(ev.currentTarget).closest('li');
-      return this.editTaskView(tg);
+      tg = $(ev.currentTarget);
+      return this.editRender(tg);
     };
 
-    CategoryListView.prototype.render = function() {
-      return $('#todos', this.el).html(_.template(this.template, {
-        'categories': this.collection.toJSON()
-      }));
+    RightNow.prototype.connect = function() {};
+
+    RightNow.prototype.render = function() {
+      $('#todos').html(this.cat_render(this.todos));
+      $('div.category').on('click', this.showAddButton);
+      $('#newcat h1').on('click', this.showAddButton);
+      $('li.task').on('click', this.editTask);
+      return $('.addstory').on('click', this.newTask);
     };
 
-    return CategoryListView;
+    return RightNow;
 
   })();
 
   $(function() {
-    var categoryList;
-    categoryList = new CategoryList();
-    return categoryList.fetch({
-      success: function(categoryList) {
-        var view;
-        view = new CategoryListView({
-          collection: categoryList
-        });
-        return view.render();
-      }
+    var rightnow;
+    return rightnow = new Lawnchair({
+      name: 'RightNow'
+    }, function(rightnow) {
+      var handler;
+      return handler = new RightNow(rightnow);
     });
   });
 
